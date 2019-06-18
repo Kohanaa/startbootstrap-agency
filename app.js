bd46c8660c4c898119f95d8dd175e522efa988da
kohanaa <selfishever@gmail.com>
2019-06-18 18:04:34 +0300

    videos page

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const port = 3000
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 //app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/', function (req, res) {res.render('home');});
+app.get('/', function (req, res) {
+  res.render('home', {
+    title:"main",
+  });
+});
+app.get('/videos', function (req, res) {
+  res.render('videos',{
+    title:"videos"
+  });
+});
 app.use(express.static("public"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
